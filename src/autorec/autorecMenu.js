38c18b29a6de494f4e9a3fecce6e00ce4e2731a9
Bakana Baka <bakanabaka.vtt@gmail.com>
2026-09-04 03:45:46 +0000

    refactor(autorec): use direct Boolean(e.broadcast) in autorecMenu context

--- src/autorec/autorecMenu.js
+++ src/autorec/autorecMenu.js
@@ -53,7 +53,7 @@ export class AutorecMenuApplication extends BaseCrosshairMenuApplication {
         const defaultUserColor = getUserColor("#000000");
         const entries = rawEntries.map(e => ({
             ...e,
-            broadcast: Boolean(e.broadcast ?? true),
+            broadcast: Boolean(e.broadcast),
             circleFile: e.circleFile ?? DEFAULT_AUTOREC_ENTRY.circleFile,
             coneFile: e.coneFile ?? DEFAULT_AUTOREC_ENTRY.coneFile,
             rayFile: e.rayFile ?? DEFAULT_AUTOREC_ENTRY.rayFile,
